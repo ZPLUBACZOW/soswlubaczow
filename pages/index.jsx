ca9bcfeef7b6127a5ff988c9ba802b76b605dd46
Kacper Rz <kacper152007@gmail.com>
2023-06-09 16:07:56 +0200

    Update 07
    added main page

--- pages/index.jsx
+++ pages/index.jsx
@@ -6,17 +6,15 @@ const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   return (
-    <>
-      <Layout title="Strona Główna" styleName="page"></Layout>
-
+    <Layout title="Strona Główna" styleName="page">
       <main className={`container ${inter.className}`}>
-        <div className="main-title">
+        <div className="main-page__title">
           <h1>
             O MŁODZIEŻOWYM OŚRODKU WYCHOWAWCZYM W <span>LUBACZOWIE</span>
           </h1>
         </div>
 
-        <div className="main-description-first">
+        <div className="main-page__description">
           <h2>
             Młodzieżowy Ośrodek Wychowawczy przeznaczony jest dla młodzieży
             charakteryzującej się poważniejszymi zaburzeniami w funkcjonowaniu
@@ -27,8 +25,8 @@ export default function Home() {
           </h2>
           <Image src="/mow.png" alt="image" width="541" height="263"></Image>
         </div>
-        <div className="main-description-second">
-          <Image src="/boisko.png" alt="image" width="430" height="290"></Image>
+        <div className="main-page__description">
+          <Image src="/pokój.jpg" alt="image" width="430" height="290"></Image>
           <h2>
             Aktualnie nasz ośrodek świadczy opiekę całodobową, całoroczną,
             przeznaczoną dla chłopców w wieku 13 – 18 lat. MOW w Lubaczowie jest
@@ -38,7 +36,7 @@ export default function Home() {
             obowiązujących standardów i norm.
           </h2>
         </div>
-        <div className="main-description-last">
+        <div className="main-page__description">
           <h2>
             Na parterze znajduje się szkoła, w skład której wchodzi: 6
             klasopracowni przedmiotowych, wyposażonych w nowoczesne pomoce
@@ -50,6 +48,15 @@ export default function Home() {
             dydaktycznych, pokój nauczycielski. W szkole używana jest ponadto
             nowoczesna sala multimedialna.
           </h2>
+          <Image
+            src="/komputery.jpg"
+            alt="image"
+            width="430"
+            height="290"
+          ></Image>
+        </div>
+        <div className="main-page__description">
+          <Image src="/pokój.jpg" alt="image" width="430" height="290"></Image>
           <h2>
             Internat usytuowany jest na I piętrze tegoż budynku. Znajdują się
             tu: pomieszczenia 4 grup wychowawczych, w skład których wchodzą:
@@ -58,6 +65,8 @@ export default function Home() {
             specjalistyczny gabinet psychologów i pedagogów, pokój pracy
             wychowawczej.
           </h2>
+        </div>
+        <div className="main-page__description">
           <h2>
             Szkoły wchodzące w skład MOW: szkoła podstawowa dla uczniów w normie
             intelektualnej i niedostosowanych społecznie oraz z
@@ -69,6 +78,15 @@ export default function Home() {
             tynkarz oraz specjalność kucharz) oraz klasa II (specjalność murarz
             – tynkarz oraz specjalność kucharz).
           </h2>
+          <Image
+            src="/zajęcia.jpg"
+            alt="image"
+            width="430"
+            height="290"
+          ></Image>
+        </div>
+        <div className="main-page__description">
+          <Image src="/boisko.png" alt="image" width="430" height="290"></Image>
           <h2>
             Ośrodek dysponuje także własną kuchnią ze stołówką oraz pralnią.
             Wychowankowie mają do dyspozycji boisko do piłki nożnej, ręcznej,
@@ -77,6 +95,6 @@ export default function Home() {
           </h2>
         </div>
       </main>
-    </>
+    </Layout>
   )
 }
